7e8144ff35bf2501a392bc30130ffd423b5bae7d
Harsh Kumar <>
2022-10-19 16:46:25 +0530

    employee post api basic

--- routes/employes.js
+++ routes/employes.js
@@ -1,53 +1,59 @@
 const express = require('express');
+const winston = require('winston');
 const { validate } = require('../validations/employee');
 const router = express.Router();
-const sqlite3 = require('sqlite3').verbose();
-
+const sqlite3 = require('sqlite3').verbose();   
 
 router.post('/', async (req, res) => {  
-    // const { error } = validate(req.body); // result.error
-    // if (error) return res.status(400).send(error.details[0].message);
+    const { error } = validate(req.body); // result.error
+    if (error) return res.status(400).send(error.details[0].message);
 
     let db = new sqlite3.Database('./db.sqlite3', (err) => {
         if (err) {
-            console.log(err.message);
+            winston.error(err.message);
         }
-        console.log('Connected to the test database.');
+        winston.info('Connected to the employee database.');
     });
 
     
-    db.run(`CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)`, (err) => {
+    db.run(`CREATE TABLE IF NOT EXISTS employees (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)`, (err) => {
         if (err) {
-            console.log(err.message);
+            winston.error(err.message);
         }
-        console.log('Created table test');
+        winston.info('Created table employees');
     });
 
 
     let sql = `INSERT INTO employees (name) VALUES(?)`;
     db.run(sql, [req.body.name], function(err) {
         if (err) {
-            return console.log(err.message);
+            return winston.error(err.message);
         }
-        console.log(`A row has been inserted with rowid ${this.lastID}`);
+        winston.info(`A row has been inserted with rowid ${this.lastID}`);
+    });
+
+    db.close();
+    res.send(req.body);
+
+});
+
+
+router.get('/', async (req, res) => {
+    let db = new sqlite3.Database('./db.sqlite3', (err) => {
+        if (err) {
+            winston.error(err.message);
+        }
+        winston.info('Connected to the employee database.');
     });
 
     db.all(`SELECT * FROM employees`, [], (err, rows) => {
         if (err) {
             throw err;
         }
-        rows.forEach((row) => {
-            console.log(row);
-        });
+        res.send(rows);
     });
 
-
-    // close the database connection
     db.close();
-
-
-    res.send(req.body);
-
 });
 
 
--- validations/employee.js
+++ validations/employee.js
@@ -2,11 +2,16 @@ const Joi = require('joi');
 
 
 function validateEmployee(employee) {
-    console.log(employee, 'employee')
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-    return Joi.validate(employee, schema);
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+        // salary: Joi.number().required(),
+        // currency: Joi.string().required(),
+        // department: Joi.string().required(),
+        // on_contract: Joi.boolean(),
+        // sub_department: Joi.string().required()
+    });
+    
+    return schema.validate(employee);
 }
 
 exports.validate = validateEmployee;
